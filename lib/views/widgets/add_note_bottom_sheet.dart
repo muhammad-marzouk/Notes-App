fd49fff6bcf276f9cc624eaf6b71821bbcab2159
m.marzouk <muhammad.marzouk@gmail.com>
2024-03-03 05:01:11 +0200

    add border radius to Bottom Sheet

---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      width: double.infinity,
+      child: Column(
+
+        children: [
+          Text("data"),
+        ],
+      ),
+    );
+  }
+}
