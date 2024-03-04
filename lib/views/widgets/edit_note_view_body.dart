42266e6839a2a637b433fd7cdc615e8bc49c39a3
m.marzouk <muhammad.marzouk@gmail.com>
2024-03-04 20:42:54 +0200

    finish edit note view body design

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notsapp/views/widgets/custom_app_bar.dart';
+import 'package:notsapp/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -15,6 +16,14 @@ class EditNoteViewBody extends StatelessWidget {
               height: 20,
             ),
             CustomAppBar(icon: Icons.edit_rounded,title: "Edite Note"),
+            SizedBox(
+              height: 14,
+            ),
+            CustomTextField(hint: "Title"),
+            SizedBox(
+              height: 14,
+            ),
+            CustomTextField(hint: "Content",maxLine: 5,)
           ],
         ),
       ),
