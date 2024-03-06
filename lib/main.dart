05f5c64c715e8f05498efb84db4c8e60ece6d0f1
m.marzouk <muhammad.marzouk@gmail.com>
2024-03-06 16:47:53 +0200

    add registerAdapter to main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notsapp/views/notes_view.dart';
 import 'constants.dart';
+import 'models/note_model.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(KNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
 
